ef32e31959386a5baaa02bab52f56b6fe69a93ef
whitney <whitney.njogu@student.moringaschool.com>
2022-08-17 18:39:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet<=400) { 
+    return 'This one is on me!';
+  } else if (feet>2500) {
+    return 'No can do.';
+  } else if (feet>2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(City){
+  const getCity = City === "NYC" ? 'Ok, sounds good.' : 'No go.';
+  return getCity;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(msg) {
+  switch (msg) {
+case (msg = 'generous') :
+return "Thank you so much.";
+break;
+case (msg = 'not as generous'):
+return 'Thank you.';
+break;
+default:
+  return 'Bye.';
+  }
 }
